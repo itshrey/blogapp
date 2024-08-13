afea392fbe221fba40689638cf8f1e813f3359b6
itshrey <itshrey2509@gmail.com>
2024-08-14 01:31:33 +0530

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,26 @@
+import User from './../models/User.model.js';
+import bcryptjs from 'bcryptjs';
+export const signup = async(req,res)=>{
+
+    const {username, email, password} = req.body;
+
+    if(!username || !email || !password || username ===''|| email===''||password===''){
+        return res.status(400).json({message:'All field are required'});
+    }
+    const hashPassword=bcryptjs.hashSync(password,10);
+    const newUser= new User({
+        username,
+        email,
+        password:hashPassword,
+    });
+
+    try{
+        await newUser.save();
+        res.json({
+        message:"Signup successful"
+        });
+    } catch(error){
+        res.status(500).json({message:error.message });
+    };
+    
+};
--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';//to support this add "type":"module" in package.j
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
-
+import authRoutes from './routes/auth.route.js'
 dotenv.config();
 mongoose.connect(process.env.MONGO)
 .then(()=>{console.log("Mongo db is connected")})
@@ -10,10 +10,12 @@ mongoose.connect(process.env.MONGO)
     console.log(err);
 })
 const app=express();
+app.use(express.json());
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000 ")
 });
 
 app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
 
