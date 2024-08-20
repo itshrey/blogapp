72eb75ba0de03af2458e14d7fb30a547ce831145
itshrey <itshrey2509@gmail.com>
2024-08-20 19:16:02 +0530

    user api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';//to support this add "type":"module" in package.j
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js'
 dotenv.config();
 mongoose.connect(process.env.MONGO)
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGO)
 })
 const app=express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000 ")
