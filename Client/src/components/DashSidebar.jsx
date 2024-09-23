130d6ba950aa766c040bc4f2fbc5ceda8bf017d5
itshrey <itshrey2509@gmail.com>
2024-09-24 02:34:40 +0530

    adding dashboard

--- Client/src/components/DashSidebar.jsx
+++ Client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Sidebar } from 'flowbite-react';
-import { HiArrowSmRight, HiDocumentText, HiOutlineAnnotation, HiOutlineUserGroup, HiUser } from 'react-icons/hi';
+import { HiArrowSmRight, HiChartPie, HiDocumentText, HiOutlineAnnotation, HiOutlineUserGroup, HiUser } from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useDispatch,useSelector } from 'react-redux';
@@ -44,6 +44,16 @@ export default function DashSidebar() {
   return (
     <Sidebar className='w-full md:w-56'>
       <Sidebar.ItemGroup className='flex flex-col gap-1'>
+        {
+          currentUser && currentUser.isAdmin && (
+            <Link to='/dashboard?tab=dash'>
+              <Sidebar.Item active={tab==='dash'|| !tab} icon={HiChartPie} as='div'>
+                Dashboard
+              </Sidebar.Item>
+
+            </Link>
+          )
+        }
         <Link to='/dashboard?tab=profile'>
             <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor="dark" as='div'>
             Profile
