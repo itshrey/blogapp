281c77495edf093bf2035af5b507afc27e85629a
itshrey <itshrey2509@gmail.com>
2025-02-11 15:26:49 +0530

    Search otp verification fix

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -5,10 +5,6 @@ export const test =(req,res)=>{
     res.json({message:"API is working"});
 };
 
-import bcryptjs from "bcryptjs";
-import User from "../models/User.model.js";
-import { errorHandler } from "../utils/error.js";
-
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
     return next(errorHandler(403, "You are not allowed to update this user"));
